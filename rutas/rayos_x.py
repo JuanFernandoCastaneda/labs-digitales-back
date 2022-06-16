25355be5b326713be55dc55d95753e34fe9b0d23
Oscar Julián Castañeda Gutiérrez <jf.castaneda@uniandes.edu.co>
2022-06-16 16:47:18 -0500

    Websocket rayos_x

--- rutas/rayos_x.py
+++ rutas/rayos_x.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, WebSocket
 import logica_maquinas.rayos_x.rayos_x as logica
 from pydantic import BaseModel
 from datetime import datetime
@@ -18,16 +18,24 @@ class Computo(BaseModel):
 
 enrutador = APIRouter()
 
-@enrutador.post("/rayos_x/")
-async def computar(parametros: Computo, usuario = Depends(obtener_usuario_actual)):
-    logica.abrir_programa()
-    logica.abrir_interfaz_maquina()
-    logica.ingresar_parametros(parametros.corriente, parametros.tiempo, parametros.tension_arranque, 
-        parametros.tension_parada, parametros.tension_incremento, parametros.angulo_arranque, 
-        parametros.angulo_parada, parametros.angulo_incremento)
-    logica.ejecutar_parametros()
-    logica.exportar_resultados()
-    return "gg ez pz"
+@enrutador.websocket("/rayos_x/")
+async def computar(websocket: WebSocket):
+    await websocket.accept()
+    usuario = obtener_usuario_actual(await websocket.receive_text())
+    if usuario is None:
+        await websocket.send("401")
+        await websocket.close()
+    else:
+        parametros = await websocket.receive_json()
+        # logica.abrir_programa()
+        # logica.abrir_interfaz_maquina()
+        # logica.ingresar_parametros(parametros.corriente, parametros.tiempo, parametros.tension_arranque, 
+        #     parametros.tension_parada, parametros.tension_incremento, parametros.angulo_arranque, 
+        #     parametros.angulo_parada, parametros.angulo_incremento)
+        # logica.ejecutar_parametros()
+        # logica.exportar_resultados()
+        await websocket.send_bytes(bin(173))
+        await websocket.close()
     
 # Esto no debería ejecutarse sino hasta el final
 @enrutador.get("/rayos_x/")
